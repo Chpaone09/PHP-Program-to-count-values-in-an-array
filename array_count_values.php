4eaaa5ec8a6988aa9ba72e6e7c8fd30a4492a287
Pawankumar Chidurala <chpaone09@hotmail.com>
2020-10-25 14:29:09 +0530

    Program to count values in an array in PHP

---
+++ array_count_values.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+***********************************************************
+
+Topic   : Program to count values in an array in PHP.
+Author  : Chpaone09®
+Date    : Oct 25, 2020
+
+************************************************************
+*/
+
+
+$a = array("a","b","a","a","b","c");
+
+echo "<pre>";print_r(array_count_values($a));
+
+?>
